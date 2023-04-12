0dc723abb948fa19a883ff4b056293e78ae572a9
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-04-12 09:31:43 -0300

    Create ex 11.py

---
+++ 1 Periodo/Listas de Exercicios/Lista 01 - 06.04.2022/ex 11.py
@@ -0,0 +1,12 @@
+'''
+11. Escreva um programa que lê um valor em radianos e o converte para Radiano.
+'''
+
+numeroRadiano = float(input('Escolha um valor em radianos para ser convertido em graus: '))
+pi = 3.141593
+
+numeroGrau = numeroRadiano * 180 / pi
+
+print(f'O número {numeroRadiano} radianos é equivalente a {numeroGrau}°')
+
+#github.com/tiagodefendi
