b0a6bade9f63f4cd51547ff879e2b147da381187
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-04-13 10:19:18 -0300

    Create ex 03.py

---
+++ 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 03.py
@@ -0,0 +1,12 @@
+'''
+3. Escreva um programa que lê um número inteiro e informa se o mesmo é par ou ímpar.
+'''
+
+numero = int(input('Escolha um valor para um número: '))
+
+if numero % 2 == 0:
+    print(f'O número {numero} é par')
+else:
+    print(f'O número {numero} é ímpar')
+
+#github.com/tiagodefendi
