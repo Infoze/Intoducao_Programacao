02c843c9cbba54ae8719625bf7e6f96fc1fdb01d
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-04-13 19:23:28 -0300

    Create ex 09.py

---
+++ 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 09.py
@@ -0,0 +1,23 @@
+'''
+9. Escreva um programa que lê três números e os escreve em ordem crescente.
+'''
+
+numero1 = int(input('Escolha um valor para o 1º número'))
+numero2 = int(input('Escolha um valor para o 2º número'))
+numero3 = int(input('Escolha um valor para o 3º número'))
+
+maior = numero1
+menor = numero1
+meio = numero1
+
+if maior < numero2:
+    maior = numero2
+if maior < numero3:
+    maior = numero3
+    
+if menor > numero2:
+    menor = numero2
+if menor > numero3:
+    menor = numero3 
+
+#github.com/tiagodefendi
