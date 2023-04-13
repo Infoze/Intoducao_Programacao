ec797635cd9a0b411f948b49111e3b1da9bf956f
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-04-13 10:41:41 -0300

    .

---
+++ 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 05.py
@@ -0,0 +1,12 @@
+'''
+5. Escreva um programa que lê dois números naturais e informa o maior. O programa também deve
+informar se os números são iguais. Caso o utilizador entre com números negativos, o programa
+deve informar um erro e não realizar as demais verificações.
+'''
+
+numero1 = int(input('Escolha um valor para o 1º número: '))
+numero2 = int(input('Escolha um valor para o 2º número: '))
+
+
+
+#github.com/tiagodefendi
