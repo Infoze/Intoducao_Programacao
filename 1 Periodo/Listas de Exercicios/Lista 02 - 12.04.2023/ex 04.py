3e12b9253f08929fa609843b5939b2bd08702427
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-04-13 10:38:45 -0300

    burrice .^.

--- 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 04.py
+++ 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 04.py
@@ -4,26 +4,26 @@
 
 numero = int(input('Escolha um valor para o número: '))
 
-if x >= 0 and x <=9:
-    if x == 0:
+if numero >= 0 and numero <=9:
+    if numero == 0:
         print(f'O número {numero} escrito por extenso é: Zero')
-    if x == 1:
+    elif numero == 1:
         print(f'O número {numero} escrito por extenso é: Um')
-    if x == 2:
+    elif numero == 2:
         print(f'O número {numero} escrito por extenso é: Dois')
-    if x == 3:
+    elif numero == 3:
         print(f'O número {numero} escrito por extenso é: Três')
-    if x == 4:
+    elif numero == 4:
         print(f'O número {numero} escrito por extenso é: Quatro')
-    if x == 5:
+    elif numero == 5:
         print(f'O número {numero} escrito por extenso é: Cinco')
-    if x == 6:
+    elif numero == 6:
         print(f'O número {numero} escrito por extenso é: Seis')
-    if x == 7:
+    elif numero == 7:
         print(f'O número {numero} escrito por extenso é: Sete')
-    if x == 8:
+    elif numero == 8:
         print(f'O número {numero} escrito por extenso é: Oito')
-    if x == 9:
+    else:
         print(f'O número {numero} escrito por extenso é: Nove')
 else:
     print(f'ERRO! O número {numero} não está no intervalo [0, 9]')
