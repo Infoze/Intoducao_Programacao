4ef44fafcbe1df7d0362f823dd6788b7041035f3
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-04-13 10:52:36 -0300

    Update ex 05.py

--- 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 05.py
+++ 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 05.py
@@ -7,6 +7,13 @@ deve informar um erro e não realizar as demais verificações.
 numero1 = int(input('Escolha um valor para o 1º número: '))
 numero2 = int(input('Escolha um valor para o 2º número: '))
 
-
+if numero1 < 0 or numero2 < 0:
+    print(f'ERRO! Somente números NATURAIS: {numero1}; {numero2}')
+elif numero1 == numero2:
+    print(f'Os números {numero1} e {numero2} são iguais')
+elif numero1 > numero2:
+    print(f'O número {numero1} é maior que {numero2}')
+else:
+    print(f'O número {numero2} é maior que {numero1}')
 
 #github.com/tiagodefendi
