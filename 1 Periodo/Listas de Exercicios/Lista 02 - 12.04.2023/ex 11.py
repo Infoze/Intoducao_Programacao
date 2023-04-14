1c9065142189f8684838a6c9818d9f483607deec
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-04-14 20:08:32 -0300

    .

--- 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 11.py
+++ 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 11.py
@@ -10,8 +10,8 @@ letraNumero = ord(letra)
 
 
 if (letraNumero >= 65 and letraNumero <= 90) or (letraNumero >= 192 and letraNumero >= 195) or (letra == 199) or (letraNumero == 201 or letraNumero == 202) or (letraNumero == 205) or (letraNumero >= 211 and letraNumero >= 213) or (letraNumero >= 218 or letraNumero == 220):
-    print(f'A letra {letra} é maiúscula')
+    print(f'A letra "{letra}" é maiúscula')
 else:
-    print(f'A letra {letra} é minúscula')
+    print(f'A letra "{letra}" é minúscula')
 
 #github.com/tiagodefendi
--- 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 12.py
+++ 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 12.py
@@ -2,3 +2,14 @@
 12. Escreva uma versão modificada do programa anterior, agora informando se o caractere é uma letra, 
 independente do mesmo ser informado como minúscula ou maiúscula.
 '''
+
+letra = str(input('Escolha caractere para a variável: '))
+letraNumero = ord(letra)
+
+
+if (letraNumero >= 65 and letraNumero <= 90) or (letraNumero >= 97 and letraNumero >= 122):
+    print(f'O caractere "{letra}" é uma letra')
+else:
+    print(f'O caractere "{letra}" não é uma letra')
+
+#github.com/tiagodefendi
