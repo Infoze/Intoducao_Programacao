6486a1ab4250c532bb3f2bf78bf21ddb4bbac99f
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-04-14 20:01:53 -0300

    Update ex 11.py

--- 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 11.py
+++ 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 11.py
@@ -5,4 +5,13 @@ a. Considere como 'letra', todos os caracteres da língua portuguesa na tabela U
 https://usefulwebtool.com/characters-portuguese
 '''
 
+letra = str(input('Escolha UMA letra para o caractere: '))
+letraNumero = ord(letra)
+
+
+if (letraNumero >= 65 and letraNumero <= 90) or (letraNumero >= 192 and letraNumero >= 195) or (letra == 199) or (letraNumero == 201 or letraNumero == 202) or (letraNumero == 205) or (letraNumero >= 211 and letraNumero >= 213) or (letraNumero >= 218 or letraNumero == 220):
+    print(f'A letra {letra} é maiúscula')
+else:
+    print(f'A letra {letra} é minúscula')
+
 #github.com/tiagodefendi
