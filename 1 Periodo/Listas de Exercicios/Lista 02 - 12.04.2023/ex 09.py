bdf6290aef46afc9a9db251c8bbcdc500b02195e
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-04-14 19:23:31 -0300

    Update ex 09.py

--- 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 09.py
+++ 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 09.py
@@ -2,24 +2,26 @@
 9. Escreva um programa que lê três números e os escreve em ordem crescente.
 '''
 
-numero1 = int(input('Escolha um valor para o 1º número'))
-numero2 = int(input('Escolha um valor para o 2º número'))
-numero3 = int(input('Escolha um valor para o 3º número'))
+numero1 = int(input('Escolha um valor para o 1º número: '))
+numero2 = int(input('Escolha um valor para o 2º número: '))
+numero3 = int(input('Escolha um valor para o 3º número: '))
 
-maior = numero1
-menor = numero1
-meio = numero1
-
-if maior < numero2:
-    maior = numero2
-if maior < numero3:
-    maior = numero3
+if (numero1 >= numero2) and (numero2 >= numero3): #1 > 2 > 3
+    print(f'A sequência em ordem crescente dos valores [{numero1, numero2, numero3}] é {numero1} >= {numero2} >= {numero3}')
     
-if menor > numero2:
-    menor = numero2
-if menor > numero3:
-    menor = numero3 
-
-
+if (numero1 >= numero3) and (numero3 >= numero2):#1 > 3 > 2
+    print(f'A sequência em ordem crescente dos valores [{numero1, numero2, numero3}] é {numero1} >= {numero3} >= {numero2}')
+    
+if (numero2 >= numero1) and (numero1 >= numero3):#2 > 1 > 3
+    print(f'A sequência em ordem crescente dos valores [{numero1, numero2, numero3}] é {numero2} >= {numero1} >= {numero3}')
+    
+if (numero2 >= numero3) and (numero3 >= numero1):#2 > 3 >1
+    print(f'A sequência em ordem crescente dos valores [{numero1, numero2, numero3}] é {numero2} >= {numero3} >= {numero1}')
+    
+if (numero3 >= numero1) and (numero1 >= numero2):#3 > 1 > 2
+    print(f'A sequência em ordem crescente dos valores [{numero1, numero2, numero3}] é {numero3} >= {numero1} >= {numero2}')
+    
+if (numero3 >= numero2) and (numero2 >= numero1):#3 > 2 > 1
+    print(f'A sequência em ordem crescente dos valores [{numero1, numero2, numero3}] é {numero3} >= {numero2} >= {numero1}')
 
 #github.com/tiagodefendi
