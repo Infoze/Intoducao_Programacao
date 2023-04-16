1a646ef052b3f63db09d8a365a7379852b368ab1
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-04-16 13:12:06 -0300

    Update ex 01.py

--- 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 01.py
+++ 1 Periodo/Listas de Exercicios/Lista 02 - 12.04.2023/ex 01.py
@@ -8,6 +8,8 @@ d. Valor hexadecimal (%X).
 
 numero = int(input('Escolha um valor para o número: '))
 
-print()
-
+print(f'O valor de {numero} na tabela ASCII é igual a {ord(numero)}')
+print(f'O valor de {numero} em Valor Decimal é igual a {numero} ')
+print(f'O valor de {numero} em OctalASCII é igual a {oct(numero)}')
+print(f'O valor de {numero} em ASCII é igual a {hex(numero)}')
 #github.com/tiagodefendi
