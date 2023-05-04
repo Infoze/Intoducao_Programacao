f83d36a0372857d7a1d51cdda5af71585f5850d8
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-05-03 22:30:16 -0300

    this is correct

--- 1 Periodo/Listas de Exercicios/Lista 03 - 26.04.2023/ex 21.py
+++ 1 Periodo/Listas de Exercicios/Lista 03 - 26.04.2023/ex 21.py
@@ -37,19 +37,34 @@ while numero != 0:
     numero = int(input('Escolha um valor para o número: '))
 
 maior = votosCandidato1
+maiorCandidato = 1
 if votosCandidato2 > maior:
     maior = votosCandidato2
+    maiorCandidato = 2
 if votosCandidato3 > maior:
     maior = votosCandidato2
+    maiorCandidato = 3
 if votosCandidato4 > maior:
     maior = votosCandidato2
+    maiorCandidato = 4
 
-print(f'Candidato1 = {votosCandidato1}')
-print(f'Candidato2 = {votosCandidato2}')
-print(f'Candidato3 = {votosCandidato3}')
-print(f'Candidato4 = {votosCandidato4}')
+if maiorCandidato == 1:
+    print(f'Candidato1 = {votosCandidato1 + votosBrancos}')
+else:
+    print(f'Candidato1 = {votosCandidato1}')
+if maiorCandidato == 2:
+    print(f'Candidato2 = {votosCandidato2 + votosBrancos}')
+else:
+    print(f'Candidato1 = {votosCandidato2}')
+if maiorCandidato ==3:
+    print(f'Candidato3 = {votosCandidato3 + votosBrancos}')
+else:
+    print(f'Candidato1 = {votosCandidato3}')
+if maiorCandidato == 4:
+    print(f'Candidato4 = {votosCandidato4 + votosBrancos}')
+else:
+    print(f'Candidato1 = {votosCandidato4}')
 print(f'Votos Nulos = {votosNulos}')
 print(f'Votos Brancos = {votosBrancos}')
-print(f'Mais votado + Brancos = {maior + votosBrancos}')
 
 #github.com/tiagodefendi
