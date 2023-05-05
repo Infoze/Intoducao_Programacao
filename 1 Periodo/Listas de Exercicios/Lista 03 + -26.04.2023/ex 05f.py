7ca9af536a677e33a172bf76a29ba11eb75bb8ea
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-05-04 22:42:06 -0300

    Create ex 05f.py

---
+++ 1 Periodo/Listas de Exercicios/Lista 03 + -26.04.2023/ex 05f.py
@@ -0,0 +1,26 @@
+'''
+5. Escreva um programa que, dado um número inteiro positivo, verifica se o mesmo é um Número
+Perfeito. Um número perfeito é igual à soma de seus divisores positivos. Exemplo: 6 = 1 + 2 + 3,
+Informe o numero: 6
+Divisores de 6: 1 + 2 + 3 = 6
+Numero perfeito? Sim
+'''
+
+numero = int(input('Escolha um valor para o número: '))
+
+soma = 0
+
+print(f'{numero}: ')
+
+for divisor in range(1, numero):
+    if numero % divisor == 0:
+        print(divisor, end=' + ')
+        soma += divisor
+print(f'\b\b= {soma}')
+
+if soma == numero:
+    print(f'O número {numero} é perfeito')
+else:
+    print(f'O número não é perfeito')
+
+#github.com/tiagodefendi
